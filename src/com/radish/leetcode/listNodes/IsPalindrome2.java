0390685f777eed15fccb3a8347c82bbe4378cf74
zhangqian <zhangqian@188yd.com>
2020-04-07 19:41:39 +0800

    今日练习

---
+++ src/com/radish/leetcode/listNodes/IsPalindrome2.java
@@ -0,0 +1,14 @@
+package com.radish.leetcode.listNodes;
+
+public class IsPalindrome2 {
+    public boolean isPalindrome(ListNode head) {
+
+        boolean flag = true;
+
+        //双指针
+        //先找到中点，翻转后半段？再进行比较？
+        //可不可以从中点直接比较？不行？不能往前，嗷！可以用栈哎！前一半都塞到栈里头，从中点开始弹栈！哇！机制！不过要开辟新空间。
+        //
+        return false;
+    }
+}
