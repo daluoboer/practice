e7505baffe98a990121a3573cad30e992b51d8ea
zhangqian <zhangqian@188yd.com>
2020-04-22 20:10:27 +0800

    e

---
+++ src/com/radish/leetcode/queue/TestQueue.java
@@ -0,0 +1,16 @@
+package com.radish.leetcode.queue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * @description TestQueue
+ * @Author Radish
+ * @Date 2020-04-22 18:54:39
+ */
+
+public class TestQueue {
+    public static void main(String[] args) {
+        ArrayBlockingQueue<String> arrayBlockingQueue = new ArrayBlockingQueue<>(20);
+
+    }
+}
