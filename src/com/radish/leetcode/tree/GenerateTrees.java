94058a4519a5ef28038d73b8c65063f5700dfacc
zhangqian <zhangqian@188yd.com>
2020-07-27 16:30:22 +0800

    不会做的题...

---
+++ src/com/radish/leetcode/tree/GenerateTrees.java
@@ -0,0 +1,50 @@
+package com.radish.leetcode.tree;
+
+import java.util.List;
+
+/**
+ * @description GenerateTrees
+ *
+ *  不同的二叉搜索树 II
+ * 给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+ *
+ *
+ *
+ * 示例：
+ *
+ * 输入：3
+ * 输出：
+ * [
+ *   [1,null,3,2],
+ *   [3,2,null,1],
+ *   [3,1,null,null,2],
+ *   [2,1,3],
+ *   [1,null,2,null,3]
+ * ]
+ * 解释：
+ * 以上的输出对应以下 5 种不同结构的二叉搜索树：
+ *
+ *    1         3     3      2      1
+ *     \       /     /      / \      \
+ *      3     2     1      1   3      2
+ *     /     /       \                 \
+ *    2     1         2                 3
+ *
+ *
+ * 提示：
+ *
+ * 0 <= n <= 8
+ *
+ * @Author Radish
+ * @Date 2020-07-27 14:04:24
+ */
+
+public class GenerateTrees {
+    public List<TreeNode> generateTrees(int n) {
+        /*二叉搜索树：左子树小，右子树大，给个n你自己生成几棵树
+        这个问题咋似曾相识呢
+
+         */
+        return null;
+    }
+}
